4698707ea713f9b2ed192c3e772aa8481c31476f
claim.second <claim.second.gmail.com>
2025-06-03 09:45:36 +0300

    Add initial Nix flake configuration for NixOS with Home Manager and i3

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "NixOS + Home Manager + i3 + nvim + flakes";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./hosts/default.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.claim = import ./home/claim.nix;
+        }
+      ];
+    };
+  };
+}
